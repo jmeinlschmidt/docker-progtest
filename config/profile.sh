e08bdcf9678914643e1cf7359ce20d46a3a03eb1
jmeinlschmidt <jakub@meinlschmidt.com>
2020-02-16 22:42:55 +0100

    fixed dircolors bug

--- config/profile.sh
+++ config/profile.sh
@@ -2,7 +2,7 @@
 export PS1='\[\e[0;33m\]\u\[\e[0m\]@\[\e[0;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w\[\e[0m\]\$ '
 
 export LS_OPTIONS='--color=auto'
-eval "`dircolors`"
+# eval "`dircolors`"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'

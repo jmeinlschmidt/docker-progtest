54fe0102f0c4a382c04ef607324579c08f0e764d
jmeinlschmidt <jakub@meinlschmidt.com>
2020-03-12 14:49:38 +0100

    added git prompt and completion

--- config/profile.sh
+++ config/profile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-export PS1='\[\e[0;33m\]\u\[\e[0m\]@\[\e[0;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w\[\e[0m\]\$ '
+source /etc/bash_completion.d/git-prompt
+source /usr/share/bash-completion/completions/git
+export PS1='\[\e[0;33m\]\u\[\e[0m\]@\[\e[0;32m\]\h\[\e[0m\]:\[\e[0;34m\]\w\[\e[0;33m\]$(__git_ps1)\[\e[0m\]\$ '
 
 export LS_OPTIONS='--color=auto'
 
 # Temporarily disabled
 # eval "`dircolors`"
 
+# Colors!
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'

f3971f557443c069ae01b2feed813f1f2936e688
jmeinlschmidt <jakub@meinlschmidt.com>
2020-03-16 18:37:34 +0100

    removed custom username

--- config/user.sh
+++ config/user.sh
@@ -3,15 +3,15 @@ set -e
  
 printf "\n\033[0;44m---> Creating SSH user.\033[0m\n"
 
-useradd -m -d /home/${SSH_USER_USERNAME} -G ssh ${SSH_USER_USERNAME} -s /bin/bash
-echo "${SSH_USER_USERNAME}:${SSH_USER_PASSWD}" | chpasswd
-echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_USER_USERNAME}/.profile
- 
-echo "${SSH_USER_USERNAME} ALL=NOPASSWD:/bin/rm" >> /etc/sudoers
-echo "${SSH_USER_USERNAME} ALL=NOPASSWD:/bin/mkdir" >> /etc/sudoers
-echo "${SSH_USER_USERNAME} ALL=NOPASSWD:/bin/chown" >> /etc/sudoers
-echo "${SSH_USER_USERNAME} ALL=NOPASSWD:/usr/sbin/useradd" >> /etc/sudoers
-echo "${SSH_USER_USERNAME} ALL=NOPASSWD:/usr/sbin/deluser" >> /etc/sudoers
-echo "${SSH_USER_USERNAME} ALL=NOPASSWD:/usr/sbin/chpasswd" >> /etc/sudoers
+usermod -a -G ssh progtest
+echo "progtest:1234" | chpasswd
+echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/progtest/.profile
+
+echo "progtest ALL=NOPASSWD:/bin/rm" >> /etc/sudoers
+echo "progtest ALL=NOPASSWD:/bin/mkdir" >> /etc/sudoers
+echo "progtest ALL=NOPASSWD:/bin/chown" >> /etc/sudoers
+echo "progtest ALL=NOPASSWD:/usr/sbin/useradd" >> /etc/sudoers
+echo "progtest ALL=NOPASSWD:/usr/sbin/deluser" >> /etc/sudoers
+echo "progtest ALL=NOPASSWD:/usr/sbin/chpasswd" >> /etc/sudoers
  
 exec "$@"
